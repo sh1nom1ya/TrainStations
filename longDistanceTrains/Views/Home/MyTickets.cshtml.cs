40388b1d173d6f974e02d64b7f7feaf2ab629767
sh1nom1ya <neongold5270@gmail.com>
2025-01-19 14:34:51 +0300

    forViews

---
+++ longDistanceTrains/Views/Home/MyTickets.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace trains.Models.Views.Home;
+
+public class MyTickets : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ longDistanceTrains/Views/Schedule/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace trains.Models.Views.Schedule;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
