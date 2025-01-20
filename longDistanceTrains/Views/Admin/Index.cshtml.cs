30326c776b9241d8dfef485a29126f91abf78cdd
sh1nom1ya <neongold5270@gmail.com>
2025-01-20 11:33:03 +0300

    addPaymentIndex

---
+++ longDistanceTrains/Views/Admin/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace trains.Models.Views.Admin;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ longDistanceTrains/Views/Payment/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace trains.Models.Views.Payment;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
